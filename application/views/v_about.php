eb4c83b59e7c760ca26343393102810439837141
koran2002 <piambreen@gmail.com>
2021-10-10 17:23:07 +0700

    tugas pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,48 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Ferdy Suprapto </li>
+        <li>Nama Belakang : Putra</li>
+    </ul>
+    <br>
+    <h4>Usia</h4>
+    <ul type="none">
+        <li>19 tahun</li>
+    </ul>
+    <br>
+    <h4>Jenis Kelamin</h4>
+    <ul type="none">
+        <li>Laki-laki</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jl. Agung barat 12 blok B 6 no 1 komplek sekneg RT 004 RW 010 kel. Sunter Agung kec. Tanjung Priok Jakarta Utara</li>
+    </ul>
+    <br>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <br>
+    <h4>Tanggal Lahir</h4>
+    <ul type="none">
+        <li>26 Maret 2002</li>
+    </ul>
+    <br>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Renang</li>
+        <li>Hiking</li>
+    </ul>
+    <br>
+    <h4>Sosial Media</h4>
+    <ul type="none">
+        <li><a href="https://www.instagram.com/ferdy_edann/">instagram</a></li>
+        <li><a href="https://twitter.com/duku1mangkok">twitter</a></li>
+        <li><a href="https://github.com/koran2002/wp2_ferdy">github</a></li>
+        <li><a href="https://www.youtube.com/c/pepewtopxin">youtube</a></li>
+    </ul>
+</section
---
+++ application/views/v_header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>web programing 2</title>
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/stylebuku.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <hgroup>
+                <h1>rentalbuku.net</h1>
+                <h3>membuat template sederhana dengan codeigniter 3</h3>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo base_url(), 'web' ?>">Home</a></li>
+                    <li><a href="<?php echo base_url(), 'web/about' ?>">About</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+        </header>
